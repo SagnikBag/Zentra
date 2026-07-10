4929cc7a55044acdac0ab918bca14be4e82a4cf9
sagnik bag <sagnikbag2001@gmail.com>
2026-07-10 22:16:17 +0530

    create controller,model,validator,routes

--- Backend/src/model/user.model.js
+++ Backend/src/model/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -24,6 +25,17 @@ const userSchema = new mongoose.Schema({
         default: 'buyer'
     }
 })
+
+userSchema.pre('save', async function(){
+    if(!this.isModified('password')) return;
+
+    const hash = await bcrypt.hash(this.password,10);
+    this.password = hash;
+})
+
+userschema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
 
 const userModel = mongoose.model("User", userSchema);
 
---
+++ Backend/src/validator/auth.validator.js
@@ -0,0 +1,30 @@
+import {body,validationResult} from 'express-validator';
+
+
+function validateRequest(req,res,next){
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+    next();
+}
+
+
+export const validateRegisterUser = [
+    body('email')
+        .isEmail().withMessage('Please provide a valid email address'),
+    body('contact')
+        .notEmpty().withMessage('Contact number is required')
+        .matches(/^[0-9]{10}$/).withMessage('Contact number must be 10 digits'),
+    body('password')
+        .notEmpty().withMessage('Password is required')
+        .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    body('fullname')
+        .notEmpty().withMessage('Full name is required')
+        .isLength({ min: 3 }).withMessage('Full name must be at least 3 characters long'),
+    validateRequest
+
+]
