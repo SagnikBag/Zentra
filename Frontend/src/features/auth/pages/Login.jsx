ab279f1d31298cb9686c64ac1b5bb6c87ce1d3e6
sagnik bag <sagnikbag2001@gmail.com>
2026-08-02 00:00:37 +0530

    chnage in login page

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -31,7 +31,7 @@ export function Login() {
                 }
                 else if (user.role == 'seller') {
 
-                    navigate('/seller/dashboar')
+                    navigate('/seller/dashboard')
                 }
             }, 1500);
 
