fa327871ce35b197cc78374a3e7caab413fc3f44
sagnik bag <sagnikbag2001@gmail.com>
2026-08-21 16:27:40 +0530

    Add favicon, icons, and index.html for frontend setup
    - Added favicon.svg for the website's favicon.
    - Created icons.svg containing various SVG icons for use in the application.
    - Introduced index.html as the main entry point for the frontend, linking to styles and scripts.
    - Configured Express to serve static files from the 'public' directory.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -21,6 +21,7 @@ app.use(cors({
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true
 }))
+app.use(express.static('public'));
 app.use(passport.initialize());
 passport.use(new GoogleStrategy({
     clientID:config.GOOGLE_CLIENT_ID,
